ebc50324594ac2d9119f0fbfa64e72487fd94814
Orkhan Hajizada <h.orkhan1907@gmail.com>
2019-05-29 04:17:03 +0400

    blog category CRUD done/admin CRUD done

---
+++ PsychologyCenter/Areas/Manage/Controllers/AdminsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using PsychologyCenter.Areas.Manage.Models;
+using PsychologyCenter.DAL;
+
+namespace PsychologyCenter.Areas.Manage.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private PsychologyContext db = new PsychologyContext();
+
+        // GET: Manage/Admins
+        public ActionResult Index()
+        {
+            return View(db.Admins.ToList());
+        }
+        
+
+        // GET: Manage/Admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manage/Admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Fullname,Email,Password")] Admin admin)
+        {
+            if (db.Admins.FirstOrDefault(a => a.Email == admin.Email) != null)
+            {
+                ModelState.AddModelError("Email", "This Email already use!");
+            }
+
+            admin.Password = Crypto.HashPassword(admin.Password);
+
+
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: Manage/Admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Manage/Admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Fullname,Email,Password")] Admin admin)
+        {
+            admin.Password = Crypto.HashPassword("Password");
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: Manage/Admins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Manage/Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Admin admin = db.Admins.Find(id);
+            db.Admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PsychologyCenter/Areas/Manage/Controllers/BlogCategoriesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PsychologyCenter.DAL;
+using PsychologyCenter.Models;
+
+namespace PsychologyCenter.Areas.Manage.Controllers
+{
+    public class BlogCategoriesController : Controller
+    {
+        private PsychologyContext db = new PsychologyContext();
+
+        // GET: Manage/BlogCategories
+        public ActionResult Index()
+        {
+            return View(db.BlogCategories.ToList());
+        }
+
+        
+        // GET: Manage/BlogCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manage/BlogCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] BlogCategory blogCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BlogCategories.Add(blogCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogCategory);
+        }
+
+        // GET: Manage/BlogCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogCategory blogCategory = db.BlogCategories.Find(id);
+            if (blogCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogCategory);
+        }
+
+        // POST: Manage/BlogCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] BlogCategory blogCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogCategory);
+        }
+
+        // GET: Manage/BlogCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogCategory blogCategory = db.BlogCategories.Find(id);
+            if (blogCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogCategory);
+        }
+
+        // POST: Manage/BlogCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogCategory blogCategory = db.BlogCategories.Find(id);
+            db.BlogCategories.Remove(blogCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
